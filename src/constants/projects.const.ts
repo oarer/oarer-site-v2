2f899630e9eb4db1f5c42765f143246aed0525e3
oarer <116757361+oarer@users.noreply.github.com>
2026-04-02 23:29:04 +0300

    Update projects.const.ts

--- src/constants/projects.const.ts
+++ src/constants/projects.const.ts
@@ -1,17 +1,17 @@
 export const projects = [
   {
+    title: 'StalHub',
+    href: 'https://stalhub.tech',
+    version: '/ v1',
+    statusText: 'In development',
+    statusColor: 'oklch(82.8% 0.189 84.429)',
+  },
+  {
     title: 'Биография',
     href: '/',
     version: '/ v2',
     statusText: 'Done, getting updates',
     statusColor: '#07b97a',
-  },
-  {
-    title: 'GribMine',
-    href: 'https://www.gribmine.ru',
-    version: '/ v1',
-    statusText: 'Not supported anymore',
-    statusColor: '#c92e2e',
   },
   {
     title: 'ContentTime',
